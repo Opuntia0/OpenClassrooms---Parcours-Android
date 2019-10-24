212173670d62420c6557d57246a062f37a9f4b6f
Mathilde <opuntia0@gmail.com>
2019-10-24 11:00:55 +0200

    Fin P2

--- Projet_Analysez_les_tests_dune_application/MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
+++ Projet_Analysez_les_tests_dune_application/MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
@@ -3,7 +3,6 @@ package com.openclassrooms.magicgithub.api;
 import com.openclassrooms.magicgithub.model.User;
 import java.util.List;
 import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.generateUsers;
-
 public class FakeApiService implements ApiService {
 
     private List<User> users = generateUsers();
@@ -14,8 +13,7 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public List<User> getUsers() {
-        // TODO: A modifier
-        return null;
+        return users;
     }
 
     /**
@@ -24,7 +22,7 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void generateRandomUser() {
-        // TODO: A modifier
+        users.add(User.random());
     }
 
     /**
@@ -32,6 +30,6 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void deleteUser(User user) {
-        // TODO: A modifier
+        users.remove(user);
     }
 }
